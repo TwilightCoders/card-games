3090d54fd9f41bb97ff7a4f8917bf8df7d892631
Jimmy Van Veen <jimmy.van.veen@iracing.com>
2019-09-27 17:08:29 -0400

    Start from scratch

--- src/components/Game/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { PlayersContext } from '../../contexts/Players';
-import Game from './Game';
-
-export default props => (
-  <PlayersContext.Consumer>
-    {({images, colorOptions, players, updatePlayers}) => <Game {...props} players={players} updatePlayers={updatePlayers} />}
-  </PlayersContext.Consumer>
-);
